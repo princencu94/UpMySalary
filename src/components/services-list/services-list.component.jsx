bf27586280419bd609608a0ce301699beac9ad73
princencu94 <56699089+princencu94@users.noreply.github.com>
2022-01-22 08:02:57 -0500

    Changing a to Link

--- src/components/services-list/services-list.component.jsx
+++ src/components/services-list/services-list.component.jsx
@@ -1,27 +1,27 @@
-
+import { Link } from 'react-router-dom'
 const ServicesList = () => {
     return (
         <div className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-6">
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1 blue-bg rounded-md py-3 px-2 hover:drop-shadow-2xl">
-            <p className="text-sm text-center font-medium text-white"><a>Elite Performer Coaching Program</a></p>
+            <p className="text-sm text-center font-medium text-white"><Link >Elite Performer Coaching Program</Link></p>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1 blue-bg rounded-md py-5 px-2 hover:drop-shadow-2xl">
-            <p className="text-sm text-center font-medium text-white "><a>Group Coaching</a></p>
+            <p className="text-sm text-center font-medium text-white "><Link>Group Coaching</Link></p>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1 blue-bg rounded-md py-5 px-2 hover:drop-shadow-2xl">
-            <p className="text-sm text-center font-medium text-white "><a>Self-Walk Program</a></p>
+            <p className="text-sm text-center font-medium text-white "><Link>Self-Walk Program</Link></p>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1 blue-bg rounded-md py-5 px-2 hover:drop-shadow-2xl">
-            <p className="text-sm text-center font-medium text-white "><a>Resume Service</a></p>
+            <p className="text-sm text-center font-medium text-white "><Link>Resume Service</Link></p>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1 blue-bg rounded-md py-5 px-2 hover:drop-shadow-2xl">
-            <p className="text-sm text-center font-medium text-white "><a>Linkedin Service</a></p>
+            <p className="text-sm text-center font-medium text-white "><Link>Linkedin Service</Link></p>
           </div>
 
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1 blue-bg rounded-md py-5 px-2 hover:drop-shadow-2xl">
-            <p className="text-sm text-center font-medium text-white "><a>Mock Interviews</a></p>
+            <p className="text-sm text-center font-medium text-white "><Link>Mock Interviews</Link></p>
           </div>
         </div>
       </div>
